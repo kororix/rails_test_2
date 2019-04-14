ea994c0d594092b54593e2d509351dc9ffbb9ab0
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-14 13:43:20 +0900

    createアクションの作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,4 +6,19 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to new_blog_path, notice: 'ブログを作成しました！'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
 end
